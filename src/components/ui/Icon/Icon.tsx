2b70c36b19d221770a57d1a3419d66edf84a3ca6
Amro Mukhtar <amromuktar@gmail.com>
2024-04-16 14:24:28 +0400

    add theme and icon component

---
+++ src/components/ui/Icon/Icon.tsx
@@ -0,0 +1,17 @@
+import { Ionicons } from '@expo/vector-icons';
+import { createBox } from '@shopify/restyle';
+import { Theme, fontSize, useAppTheme } from '@/theme';
+import { IconProps } from './Icon.type';
+
+const InnerIcon = createBox<Theme>(Ionicons);
+
+export const Icon: React.FC<IconProps> = ({ isPrimary, ...rest }) => {
+  const { colors } = useAppTheme();
+  return (
+    <InnerIcon
+      color={isPrimary ? colors.primary : colors.secondary}
+      size={fontSize.l}
+      {...rest}
+    />
+  );
+};
